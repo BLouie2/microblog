8997590d8adbee62cd7937795bea041eec8ec416
Brian Louie <b.louie31@gmail.com>
2018-12-23 21:37:40 -0500

    2nd chapter work

--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,19 @@
 from flask import render_template
 from app import app
 
+
 @app.route('/')
 @app.route('/index')
 def index():
 	user = {'username':'Miguel'}
 	posts = [
-		{
-			'author': {'username': 'John'},
-			'body': 'Beautiful day in Poland!'
-		},
-		{
-			'author': {'username': 'Susan'},
-			'body':'The Avengers movie was so cool!'
-		}
-	]	
+			{
+				'author': {'username': 'John'},
+				'body': 'Beautiful day in Poland!'
+			},
+			{
+				'author': {'username': 'Susan'},
+				'body':'The Avengers movie was so cool!'
+			}
+		]	
 	return render_template('index.html',title='Home', user=user)
