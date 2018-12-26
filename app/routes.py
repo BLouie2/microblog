d612b368462f39dfe473609430e77f61e6dd3699
Brian Louie <b.louie31@gmail.com>
2018-12-26 08:47:15 -0500

    templates commit

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+from flask import Flask
 from flask import render_template
 from app import app
 
@@ -5,15 +6,15 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-	user = {'username':'Miguel'}
+	user = {'username':'Jonathan'}
 	posts = [
-			{
-				'author': {'username': 'John'},
-				'body': 'Beautiful day in Poland!'
-			},
-			{
-				'author': {'username': 'Susan'},
-				'body':'The Avengers movie was so cool!'
-			}
-		]	
-	return render_template('index.html',title='Home', user=user)
+		{
+			'author': {'username': 'John'},
+			'body': 'Beautiful day in Poland!'
+		},
+		{
+			'author': {'username': 'Susan'},
+			'body':'The Avengers movie was so cool!'
+		}
+	]
+	return render_template('index.html',title='Home', user=user, posts=posts)
