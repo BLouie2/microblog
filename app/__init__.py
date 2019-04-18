6a07a99eabd2ba024c8f9801205c206afe877077
Brian Louie <b.louie31@gmail.com>
2019-04-17 23:32:53 -0400

    I18n and L10n and flask babel translations

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -9,6 +9,7 @@ import os
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_babel import Babel, lazy_gettext as _l
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -17,9 +18,11 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 #the login value above is the name you would use for a url_for() call
+login.login_message = _l('Please log in to access this page.')
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+babel = Babel(app)
 
 if not app.debug:
 	if app.config['MAIL_SERVER']:
@@ -48,5 +51,11 @@ if not app.debug:
 
 	app.logger.setLevel(logging.INFO)
 	app.logger.info('Microblog startup')
+
+
+@babel.localeselector
+def get_locale():
+	return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 from app import routes, models, errors
